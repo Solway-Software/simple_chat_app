9c7d20ca96c7062fe2c0ab8d635d09be8107d916
Mahammad Bakr <49376835+mahammadbakr@users.noreply.github.com>
2024-03-29 15:41:03 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'simple_chat_app',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Simple Chat App'),
+          ),
+          body: const Center(
+            child: Text('Hi there! Welcome to Simple Chat App!'),
+          ),
+        ));
+  }
+}
